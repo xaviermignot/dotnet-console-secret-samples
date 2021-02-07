242ad0b4a9d632d2bfbc7b1b19310296b8a2e324
Xavier Mignot <code@xmi.fr>
2021-02-07 10:45:19 +0100

    Add host builder sample

---
+++ 03-UseHostBuilder/Program.cs
@@ -0,0 +1,53 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace UseHostBuilder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Host.CreateDefaultBuilder()
+                .ConfigureAppConfiguration((hostContext, builder) =>
+                {
+                    builder.AddUserSecrets<Program>();
+                })
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.Configure<MyConfiguration>(hostContext.Configuration.GetSection(nameof(MyConfiguration)));
+                    services.AddHostedService<ConsoleWorker>();
+                }).Build().Run();
+        }
+    }
+
+    class MyConfiguration
+    {
+        public string MyFirstSecret { get; set; }
+        public string MySecondSecret { get; set; }
+    }
+
+    class ConsoleWorker : BackgroundService
+    {
+        private readonly MyConfiguration _myConfiguration;
+        private readonly ILogger _logger;
+
+        public ConsoleWorker(IOptions<MyConfiguration> myConfiguration, ILogger<ConsoleWorker> logger)
+        {
+            _myConfiguration = myConfiguration.Value;
+            _logger = logger;
+        }
+
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            _logger.LogInformation($"The first secret is: {_myConfiguration.MyFirstSecret}");
+            _logger.LogInformation($"The second secret is: {_myConfiguration.MySecondSecret}");
+
+            return Task.CompletedTask;
+        }
+    }
+}
