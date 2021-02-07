5e12c90d7b59546147f3d32e66f58b9d74754712
Xavier Mignot <code@xmi.fr>
2021-02-07 10:44:55 +0100

    Add single value sample

---
+++ 01-SingleValueSample/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+
+namespace SingleValueSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                .AddUserSecrets<Program>()
+                .Build();
+
+            var secretValue = configuration["MySecret"];
+
+            Console.WriteLine($"The secret value is: {secretValue}");
+        }
+    }
+}
